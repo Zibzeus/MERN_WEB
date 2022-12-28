7963f91080f96077bfb5212f09d7324e32069244
zibzeus <jalantikus360@gmail.com>
2022-12-28 17:09:43 +0700

    Membuat Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Waroeng</a>
       </header>
-      <main> list </main>
+      <main>
+        <h1>Product</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.gambar} alt={product.nama} />
+              </a>
+              {console.log(product.gambar)}
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <h2>{product.nama}</h2>
+                </a>
+                <div>
+                  <strong>Rp.{product.harga}</strong>
+                </div>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
